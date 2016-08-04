8d8635e1bb2e658db9366f67dddd0b4fd92127d4
Benjamin Cooper <benjaminkagancooper@gmail.com>
2016-08-04 18:48:18 -0400

    first update with editing

--- src/containers/show_edit.js
+++ src/containers/show_edit.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { deletePost, fetchpost } from '../actions/index';
+import { deletePost, fetchpost, updatePost } from '../actions/index';
 
 // import marked from 'marked';
 
@@ -10,9 +10,14 @@ class Show extends Component {
     super(props);
 
     // init component state here
-    // this.state = { title: '', tags: '', content: '' };
-    // this.updateLocalPost = this.updateLocalPost.bind(this);
+    this.state = { title: '', tags: '', content: '', editingTitle: false, editingTags: false, editingContent: false };
     this.onDeleteButtonPress = this.onDeleteButtonPress.bind(this);
+    this.renderTitle = this.renderTitle.bind(this);
+    this.renderTags = this.renderTags.bind(this);
+    this.renderContent = this.renderContent.bind(this);
+    this.editCheckTitle = this.editCheckTitle.bind(this);
+    this.editCheckTags = this.editCheckTags.bind(this);
+    this.editCheckContent = this.editCheckContent.bind(this);
   }
 
   componentWillMount() {
@@ -25,20 +30,64 @@ class Show extends Component {
     this.props.deletePost(this.props.params.id);
   }
 
+  // if (this.state.editingTitle) {
+  //   this.state.setState({ editingTitle: false });
+  // } else {
+  //   this.state.setState({ editingTitle: true });
+  // }
+  editCheckTitle() {
+    this.setState({ editingTitle: !this.state.editingTitle });
+  }
+
+  editCheckTags() {
+    this.setState({ editingTags: !this.state.editingTags });
+  }
+
+  editCheckContent() {
+    this.setState({ editingContent: !this.state.editingContent });
+  }
+
+  renderContent() {
+    if (this.state.editingContent) {
+      return <div>EDITING CONTENT</div>;
+    } else {
+      return <div>{this.props.post.content}</div>;
+    }
+  }
+
+  renderTags() {
+    if (this.state.editingTags) {
+      return <div>EDITING TAGS</div>;
+    } else {
+      return <div>{this.props.post.tags}</div>;
+    }
+  }
+
+  renderTitle() {
+    if (this.state.editingTitle) {
+      return <div>EDITING TITLE</div>;
+    } else {
+      return <div>{this.props.post.title}</div>;
+    }
+  }
+
   render() {
     if (!this.props.post) {
-      return <div>loading...</div>;
+      return <div>loading post...</div>;
     }
     return (
       <div>
         <div>
-          {this.props.post.title}
+          {this.renderTitle()}
+          <button type="submit" onClick={this.editCheckTitle} >Edit Title</button>
         </div>
         <div>
-          {this.props.post.tags}
+          {this.renderTags()}
+          <button type="submit" onClick={this.editCheckTags} >Edit Tags</button>
         </div>
         <div>
-          {this.props.post.content}
+          {this.renderContent()}
+          <button type="submit" onClick={this.editCheckContent} >Edit Content</button>
         </div>
         <button type="submit" onClick={this.onDeleteButtonPress} >DELETE</button>
       </div>
@@ -52,4 +101,4 @@ const mapStateToProps = (state) => (
     post: state.posts.post,
   }
 );
-export default connect(mapStateToProps, { deletePost, fetchpost })(Show); // / export functions where null is
+export default connect(mapStateToProps, { updatePost, deletePost, fetchpost })(Show); // / export functions where null is
